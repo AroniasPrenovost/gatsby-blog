ab22211ca574db5e34d61ad106a73164660343fa
Aron Jon Prenovost <Aroniascoder@gmail.com>
2019-12-05 21:15:48 -0800

    updates

--- src/components/footer.js
+++ src/components/footer.js
@@ -35,13 +35,13 @@ export default ({ children }) => {
   }
 
   //   var currentPath = window.location.pathname // broken hack, need to pass props
-  var files = flattenObj(data)
-  var postCount = 0
-  files = Object.values(files)
-  for (var i = 0; i < files.length; i++) {
-    if (files[i].includes("/blog/") && files[i].length > 6) {
-      postCount++
-    }
+//   var files = flattenObj(data)
+//   var postCount = 0
+//   files = Object.values(files)
+//   for (var i = 0; i < files.length; i++) {
+//     if (files[i].includes("/blog/") && files[i].length > 6) {
+//       postCount++
+//     }
     // if (files[i].indexOf("SitePage ") !== -1) {
     //   files[i] = files[i].replace("SitePage ", "")
     //   if (currentPath === files[i]) {
@@ -53,26 +53,26 @@ export default ({ children }) => {
     // }
   }
 
-  function createP() {
-    var p = document.createElement("p")
-    p.innerText = "test"
-    return p
-  }
+//   function createP() {
+//     var p = document.createElement("p")
+//     p.innerText = "test"
+//     return p
+//   }
 
-  var ddd
-  // hack
-  if (currentPath === "/") {
-    files = [{ slug: "/blog/", name: "View all " + postCount + " blog posts" }]
-  } else if (currentPath === "/blog/") {
-    files = [{ slug: "/", name: "Home" }]
-    ddd = createP()
-    console.log(ddd)
-  } else {
-    files = [
-      { slug: "/", name: "Home" },
-      { slug: "/blog/", name: "All posts" },
-    ]
-  }
+//   var ddd
+//   // hack
+//   if (currentPath === "/") {
+//     files = [{ slug: "/blog/", name: "View all " + postCount + " blog posts" }]
+//   } else if (currentPath === "/blog/") {
+//     files = [{ slug: "/", name: "Home" }]
+//     ddd = createP()
+//     console.log(ddd)
+//   } else {
+//     files = [
+//       { slug: "/", name: "Home" },
+//       { slug: "/blog/", name: "All posts" },
+//     ]
+//   }
 
   return (
     <div>
